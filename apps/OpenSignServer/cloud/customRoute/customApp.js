535508931450a8b65ef1c62b12f4d9e14e1efb13
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2023-11-29 19:31:11 +0530

    add signpdf express route

--- apps/OpenSignServer/cloud/customRoute/customApp.js
+++ apps/OpenSignServer/cloud/customRoute/customApp.js
@@ -5,9 +5,14 @@ export const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 import uploadFile from './uploadFile.js';
-import getDocuments from './getDocuments.js'
+import getDocuments from './getDocuments.js';
+import signpdf from './signpdf.min.js';
 app.post('/file_upload', uploadFile);
-app.get('/getdocuments', getDocuments)
+app.get('/getdocuments', getDocuments);
+app.post('/signpdf', signpdf);
